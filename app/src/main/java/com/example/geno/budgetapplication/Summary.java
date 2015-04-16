efd0ac167936d8a41924996b8e21d135554b57a4
GenoGarcia <geno.garcia@ymail.com>
2015-04-16 13:55:28 -0600

    Finished...
    Maybe...

--- app/src/main/java/com/example/geno/budgetapplication/Summary.java
+++ app/src/main/java/com/example/geno/budgetapplication/Summary.java
@@ -1,28 +1,57 @@
 package com.example.geno.budgetapplication;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class Summary extends ActionBarActivity {
+
+    protected TextView total;
+    protected TextView food;
+    protected TextView shelter;
+    protected TextView utilities;
+    protected TextView clothing;
+    protected TextView transportation;
+    protected TextView medical;
+    protected TextView personal;
+    protected TextView savings;
+    protected TextView extra;
+
+    public void initializeElements() {
+        total = (TextView) findViewById(R.id.total);
+        food = (TextView) findViewById(R.id.food);
+        shelter = (TextView) findViewById(R.id.shelter);
+        utilities = (TextView) findViewById(R.id.utilities);
+        clothing = (TextView) findViewById(R.id.clothing);
+        transportation = (TextView) findViewById(R.id.transportation);
+        medical = (TextView) findViewById(R.id.medical);
+        personal = (TextView) findViewById(R.id.personal);
+        savings = (TextView) findViewById(R.id.savings);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_summary);
 
-        sharedPref.setText(amount.getText().toString() + " " + foodBudget);
-        sharedPref.setText(amount.getText().toString() + " " + shelterBudget);
-        sharedPref.setText(amount.getText().toString() + " " + utilitiesBudget);
-        sharedPref.setText(amount.getText().toString() + " " + clothingBudget);
-        sharedPref.setText(amount.getText().toString() + " " + transportationBudget);
-        sharedPref.setText(amount.getText().toString() + " " + MedicalBudget);
-        sharedPref.setText(amount.getText().toString() + " " + personalBudget);
-        sharedPref.setText(amount.getText().toString() + " " + savingsBudget);
-        //pull values from shared prefs using names and supplying default values to use if the specified values aren't in the shared prefs
-        //BreakAway in main Activity where they pull from sharedprefs
-    }
+        initializeElements();
+        SharedPreferences sharedPref = getBaseContext().getSharedPreferences("Prefs", Context.MODE_PRIVATE);
 
+        total.setText("Paycheck Amount: " + sharedPref.getFloat("Payment", 0));
+        food.setText("Food: " + sharedPref.getFloat("Food", 0));
+        shelter.setText("Shelter: " + sharedPref.getFloat("Shelter", 0));
+        utilities.setText("Utilities: " + sharedPref.getFloat("Utilities", 0));
+        clothing.setText("Clothing: " + sharedPref.getFloat("Clothing", 0));
+        transportation.setText("Transportation: " + sharedPref.getFloat("Transportation", 0));
+        medical.setText("Medical: " + sharedPref.getFloat("Medical", 0));
+        personal.setText("Personal: " + sharedPref.getFloat("Personal", 0));
+        savings.setText("Savings: " + sharedPref.getFloat("Savings", 0));
+        extra.setText("Extra: " + sharedPref.getFloat("Extra", 0));
+    }
 }
