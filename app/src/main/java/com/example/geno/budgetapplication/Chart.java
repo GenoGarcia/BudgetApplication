9b8b05065dbdf9fb8ee2026475cbbecc2a1ebbcc
GenoGarcia <geno.garcia@ymail.com>
2015-04-23 12:25:52 -0600

    Pie chart attempt
    Added pages and activities to create pie chart.

---
+++ app/src/main/java/com/example/geno/budgetapplication/Chart.java
@@ -0,0 +1,39 @@
+package com.example.geno.budgetapplication;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.SurfaceView;
+
+/**
+ * Created by Geno on 4/22/2015.
+ */
+public class Chart extends ChartSummary  {
+    private int screenWidth;
+    private int screenHeight;
+
+    //UI
+    private Paint backgroundPaint;
+    Paint circlePaint;
+    Paint sectionPaint;
+    int numberOfSections;
+    Context context;
+    Activity activity;
+
+
+    public Chart(Context context, AttributeSet attrs){
+        //Sort out the Activity stuff
+        this.context = context;
+        activity = (Activity) context;
+
+        //Set background paint
+        backgroundPaint = new Paint();
+        backgroundPaint.setColor(Color.WHITE);
+
+        //Start Game
+
+
+    }
+}
